359a3b74e0f37e1e3a27af805d7411018801a833
Ankita Mane <maneankita831@gmail.com>
2026-02-18 17:13:37 +0530

    exp 4, exp 5

--- Experiments/Exp4.js
+++ Experiments/Exp4.js
@@ -9,21 +9,29 @@ function submitButtonClick(event){
     document.getElementById("nameError").innerHTML = "";
     document.getElementById("emailError").innerHTML = "";
     document.getElementById("passwordError").innerHTML ="";
+    document.getElementById("successMSG").innerHTML = "";
 
     let isvalid = true;
-    if(name.trim() == ""){
+    if(name == ""){
         document.getElementById("nameError").innerHTML ="Name field is required";
         isvalid = false;
     }
-    if(email.trim() == "" || !email.includes("@")){
+    if(email == "" || !email.includes("@")){
         document.getElementById("emailError").innerHTML = "Enter the Correct email";
         isvalid = false;
     }
-    if(password.trim() == "" || password.length <= 6){
-        document.getElementById("passwordError").innerHTML = "Enter the 6 digit password";
+    if(password == "" || password.length < 6){
+        document.getElementById("passwordError").innerHTML = "Password must be more than 6 digit";
         isvalid = false;
     }
     if(isvalid){
-        alert("Form submitted Successfully");
+        //alert("Form submitted Successfully");
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("pass").value = "";
+        document.getElementById("successMSG").innerHTML = "Form submitted successfully";
+        document.getElementById("successMSG").style.color ="green";
+        return isvalid;
     }
+    
 }
---
+++ Experiments/Express/Exp5.js
@@ -0,0 +1,3 @@
+//npm -- node package manager
+//1.initialize node project - npm init -y
+//2.npm install express
