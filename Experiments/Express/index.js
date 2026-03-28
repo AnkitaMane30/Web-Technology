57d27bf9963da8ef50b82d6321d3a7203168abfb
Ankita Mane <maneankita831@gmail.com>
2026-03-28 23:41:35 +0530

    24 march lecture

--- Experiments/Express/index.js
+++ Experiments/Express/index.js
@@ -1,15 +1,23 @@
-const express = require('express');
+const express = require('express'); // import statement
 const app = express();
 const port = 3000;
 
 app.get("/",(req,res)=>{
     const time = Date();
-    res.json({
-    msg:"hello user",
-    time1 : time
-})
+    res.send("welcome To home page")
 })
 
+app.get("/about",(req,res)=>{
+  res.send("This is About page")
+})
+
+app.get("/contact",(req,res)=>{
+  res.send("This is Contact page")
+})
+
+app.get("/info",(req,res)=>{
+  res.send("This is information page")
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 
