58362c66aeed08c43e9c4d374b4cec1f6b8bd221
Ankita Mane <maneankita831@gmail.com>
2026-04-16 11:00:26 +0530

    Experiment 8

--- Experiments/Angular/Exp 8/StudentManagement/src/app/app.ts
+++ Experiments/Angular/Exp 8/StudentManagement/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from '../home/home';
+import { AddStudent } from '../add-student/add-student';
+import { ListStudent } from '../list-student/list-student';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ Experiments/Angular/Exp 8/StudentManagement/src/home/home.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+  student = {
+    name: "Ankita",
+    age: 21,
+    course: "AIML"
+  }
+
+  newStudent = {
+    name: '',
+    age:'',
+    course:''
+  }
+
+  updateStudent(){
+    this.student.name = this.newStudent.name;
+    this.student.age = Number(this.newStudent.age);
+    this.student.course = this.newStudent.course;
+  }
+
+    //array of courses
+  courses : string[] = ["CSE", "AIML", "AIDS", "ENTC"];
+
+}
