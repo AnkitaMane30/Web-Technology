414b27daea89a8a6cb88632dc312ab0903abf81e
Ankita Mane <maneankita831@gmail.com>
2026-04-23 00:09:56 +0530

    Service Lecture

--- Class/Angular/MyApp/src/app/app.ts
+++ Class/Angular/MyApp/src/app/app.ts
@@ -1,12 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Home } from '../home/home';
-import { AddStudent } from '../add-student/add-student';
-import { ListStudents } from '../list-students/list-students';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home,ListStudents],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
